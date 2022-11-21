3da1c81af3ab8dbb243c2317675b5d3dbd7104ca
Your Name <you@example.com>
2022-11-21 17:39:47 +0000

    .

--- requirements/mariaDB/tools/mariadb.sql
+++ requirements/mariaDB/tools/mariadb.sql
@@ -1,6 +1,6 @@
 
-CREATE DATABASE IF NOT EXISTS wordpressDB; -- craete database if not exists (name: wordpress)
+CREATE DATABASE IF NOT EXISTS wordpress; -- craete database if not exists (name: wordpress)
 CREATE USER IF NOT EXISTS 'roudouch'@'%' IDENTIFIED BY '1234'; -- create user with password if not exists
-GRANT ALL PRIVILEGES ON wordpressDB.* TO 'roudouch'@'%'; -- give all privileges to user 'roudouch'@'%' on database 'wordpress'
+GRANT ALL PRIVILEGES ON wordpress.* TO 'roudouch'@'%' WITH GRANT OPTION; -- give all privileges to user 'roudouch'@'%' on database 'wordpress'
 FLUSH PRIVILEGES; -- To apply the changes, flush the privileges.
 ALTER USER 'root'@'localhost' IDENTIFIED BY '1234';  -- change root password
