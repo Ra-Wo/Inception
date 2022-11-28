7d00fb1751f905d093f031d5b1aca8065f6c4c8f
Rachid Oudouch <Rashidoudouch@gmail.com>
2022-11-28 13:48:04 +0000

    done

--- srcs/requirements/static_page/app/app.js
+++ srcs/requirements/static_page/app/app.js
@@ -2,8 +2,6 @@ var express = require('express');
 var app = express();
 const path = require('path')
 
-app.use('/', express.static(path.join(__dirname, '/public')))
-
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
